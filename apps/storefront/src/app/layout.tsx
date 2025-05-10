c667136e2f2b93d1dc9221b9fe66e2913e037eac
Soheil Movahedan <soheil.movahhedan@gmail.com>
2025-05-10 06:03:06 +0200

    fix(repo): sonar cloud issues

--- apps/storefront/src/app/layout.tsx
+++ apps/storefront/src/app/layout.tsx
@@ -2,9 +2,9 @@ import "./styles.css";
 
 export default function RootLayout({
 	children,
-}: {
+}: Readonly<{
 	children: React.ReactNode;
-}) {
+}>) {
 	return (
 		<html lang="en">
 			<body>{children}</body>
