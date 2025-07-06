1d2158cb03464d560727fd0a10866d3496897895
Soheil Movahedan <soheil.movahhedan@gmail.com>
2025-07-06 09:17:32 +0000

    fix: small issue

--- scripts/setup/cleanup.ts
+++ scripts/setup/cleanup.ts
@@ -1,7 +1,5 @@
-import { rm } from "node:fs/promises";
 import { $ } from "bun";
 import chalk from "chalk";
-import { runnables } from "./config";
 
 async function cleanup() {
 	try {
@@ -19,33 +17,7 @@ async function cleanup() {
 		console.error("docker compose down failed:", error);
 	}
 
-	const baseDir = process.cwd();
-	const sources = [baseDir, ...runnables.map((runnable) => runnable.absPath)];
-
-	const directories = [
-		".turbo",
-		".cache",
-		".vercel",
-		".next",
-		"build",
-		"dist",
-		"out",
-		"node_modules",
-	];
-
-	for (const source of sources) {
-		console.log(chalk.green(`👋 Removing cache related files from ${source}`));
-		for (const directory of directories) {
-			try {
-				await rm(`${[source, directory].join("/")}`, {
-					recursive: true,
-					force: true,
-				});
-			} catch {}
-		}
-	}
-	console.log(chalk.green("👋 Cache related files removed successfully"));
-	console.log(chalk.green("👋 Dist related files removed successfully"));
+	await $`bun run dev:cleanup`;
 }
 
 cleanup();
