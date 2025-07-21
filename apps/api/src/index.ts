f27bf7d0b510762ea017a819a07844107f8a4925
Soheil Movahedan <soheil.movahhedan@gmail.com>
2025-07-21 00:23:10 +0000

    fix(repo): remove dependencies as a work around ci fixing

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,9 @@
-import { log } from "@repo/logger";
+// import { log } from "@repo/logger";
 import { createServer } from "./server";
 
 const port = Number(process.env.PORT) || 3004;
 const server = createServer();
 
 server.listen(port, "0.0.0.0", () => {
-	log(`api running on ${port}`);
+	console.log(`api running on ${port}`);
 });
