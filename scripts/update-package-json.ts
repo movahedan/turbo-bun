171a77ec8abbf8e29ca428856c01c642f9ba67d9
Soheil Movahedan <soheil.movahhedan@gmail.com>
2025-08-20 22:48:50 +0200

    fix(root): resolve update-package-json script issue

--- scripts/update-package-json.ts
+++ scripts/update-package-json.ts
@@ -44,8 +44,26 @@ export const updatePackageJson = createScript(
 			const newExports: Record<string, string> = {};
 
 			for (const file of files) {
-				const shouldSkip =
-					!file.isDirectory() && !file.name.endsWith(".ts") && !file.name.endsWith(".tsx");
+				if (file.isDirectory()) {
+					const indexFile = path.join(srcDir, file.name, "index.ts");
+					const indexTsxFile = path.join(srcDir, file.name, "index.tsx");
+					const sameNameFile = path.join(srcDir, file.name, `${file.name}.ts`);
+					const sameNameTsxFile = path.join(srcDir, file.name, `${file.name}.tsx`);
+
+					if (await Bun.file(indexFile).exists()) {
+						newExports[`./${file.name}`] = `./${path.relative(packageDir, indexFile)}`;
+					} else if (await Bun.file(indexTsxFile).exists()) {
+						newExports[`./${file.name}`] = `./${path.relative(packageDir, indexTsxFile)}`;
+					} else if (await Bun.file(sameNameFile).exists()) {
+						newExports[`./${file.name}`] = `./${path.relative(packageDir, sameNameFile)}`;
+					} else if (await Bun.file(sameNameTsxFile).exists()) {
+						newExports[`./${file.name}`] = `./${path.relative(packageDir, sameNameTsxFile)}`;
+					}
+
+					continue;
+				}
+
+				const shouldSkip = !file.name.endsWith(".ts") && !file.name.endsWith(".tsx");
 				if (shouldSkip) continue;
 
 				if (file.name === "index.ts") {
@@ -53,9 +71,7 @@ export const updatePackageJson = createScript(
 					continue;
 				}
 
-				const mainFile = file.isDirectory()
-					? path.join(srcDir, file.name, `${file.name}.tsx`)
-					: path.join(srcDir, file.name);
+				const mainFile = path.join(srcDir, file.name);
 				const relativePath = `./${path.relative(packageDir, mainFile)}`;
 
 				if (await Bun.file(mainFile).exists()) {
